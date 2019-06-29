6f5d2913ea929cb29c6d4f2170ad6303538f6ed8
andrewcathcart <andrew.cathcart@salecycle.com>
2019-06-29 17:41:18 +0100

    included temperature, some print formatting using dataframes, tidied up

--- weather.py
+++ weather.py
@@ -5,6 +5,59 @@ import requests
 import json
 import sys
 from datetime import datetime
+import pandas as pd
+
+
+def kelvin_to_celsius(kelvin):
+    """ Openweather returns temperature readings in Kelvin, need to convert this"""
+    return round(kelvin - 273.15, 2)
+
+
+def print_weather_summary():
+    URL = "http://api.openweathermap.org/data/2.5/weather"
+    r = requests.get(URL, params=payload).json()
+    dt = datetime.utcfromtimestamp(r["dt"] + r["timezone"])
+
+    description, temp, temp_min, temp_max = description_and_temperature(r)
+
+    df = pd.DataFrame({"Description": description, "Temp (°C)": temp, "Min (°C)": temp_min,
+                       "Max (°C)": temp_max}, index=[dt])
+
+    print(df)
+
+
+def print_forecast_summary():
+    URL = "http://api.openweathermap.org/data/2.5/forecast"
+    r = requests.get(URL, params=payload).json()
+
+    current_date = datetime.now().date()
+    forecast_list = []
+    for el in r["list"]:
+        forecast_datetime = datetime.utcfromtimestamp(
+            el["dt"])
+
+        # exit the loop after we've printed forecast information for today
+        if forecast_datetime.date() != current_date:
+            break
+
+        description, temp, temp_min, temp_max = description_and_temperature(el)
+
+        df = pd.DataFrame({"Description": description, "Temp (°C)": temp, "Min (°C)": temp_min,
+                           "Max (°C)": temp_max}, index=[forecast_datetime])
+        forecast_list.append(df)
+
+    df = pd.concat(forecast_list)
+    print(df)
+
+
+def description_and_temperature(obj):
+    main = obj["main"]
+    description = obj["weather"][0]["description"]
+    temp = kelvin_to_celsius(main["temp"])
+    temp_min = kelvin_to_celsius(main["temp_min"])
+    temp_max = kelvin_to_celsius(main["temp_max"])
+    return description, temp, temp_min, temp_max
+
 
 API_KEY = os.environ["OPENWEATHERMAP_API_KEY"]
 
@@ -12,23 +65,8 @@ payload = {
     "APPID": API_KEY,
     "q": "newcastle upon tyne,gb"
 }
-URL = "http://api.openweathermap.org/data/2.5/weather"
 
 if len(sys.argv) > 1 and sys.argv[1] == 'forecast':
-    URL = "http://api.openweathermap.org/data/2.5/forecast"
-    r = requests.get(URL, params=payload).json()
-
-    current_date = datetime.now().date()
-    for el in r["list"]:
-        forecast_datetime = datetime.utcfromtimestamp(el["dt"])
-        forecast_date = datetime.date(forecast_datetime)
-
-        # exit the loop after we've printed forecast information for today
-        if forecast_date != current_date:
-            break
-
-        weather_description = el["weather"][0]["description"]
-        print(forecast_datetime, weather_description)
+    print_forecast_summary()
 else:
-    r = requests.get(URL, params=payload).json()
-    print("Weather currently:", r["weather"][0]["description"])
+    print_weather_summary()
